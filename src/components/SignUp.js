bf43dd2ff990aea7a2c760f210fe519948c26fd6
Kevin Towner <kevin91@Kevins-MacBook-Pro.local>
2018-02-08 20:37:24 -0500

    save

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () =>
-  <div>
-    <h1>sign up</h1>
-  </div>
+	<div>
+		<h1>sign up</h1>
+		<SignUpForm />
+  	</div>
 
-export default SignUp;
+
+const INITIAL_STATE = {
+  	username: '',
+  	email: '',
+  	passwordOne: '',
+  	passwordTwo: '',
+  	error: null,
+};
+
+
+
+
+class SignUpForm extends Component {
+	constructor(props) {
+    	super(props);
+
+    	this.state = { ...INITIAL_STATE };
+  	}
+
+  	onSubmit = (event) => {
+
+  	}
+
+  	render() {
+    	return (
+      		<form onSubmit={this.onSubmit}>
+
+      		</form>
+    	);
+  	}
+}
+
+const SignUpLink = () =>
+  	<p>
+    	Don't have an account?
+    	{' '}
+    	<Link to={routes.SIGN_UP}>Sign Up</Link>
+  	</p>
+
+export default SignUpPage;
+
+export {
+  SignUpForm,
+  SignUpLink,
+};
+	
+
+export default SignUp
